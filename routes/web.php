fd8c5527157338e08111ad2ac7fc79f9f4ec436a
Will <wilbertbryan04@gmail.com>
2024-08-15 17:11:51 +0700

    Merge Design

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+Route::get('/info', function () {
+    return view('info');
+});
+Route::get('/info2', function () {
+    return view('info2');
+});
+Route::get('/kritiksaran', function () {
     return view('kritikSaran');
 });
+
+
